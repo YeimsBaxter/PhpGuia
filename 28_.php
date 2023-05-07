77baad7e48d943e9d96b62ce741c561f8f980540
YeimsBaxter <YeimsBaxter@gmail.com>
2023-05-07 11:17:46 -0500

    Se agregó la información del trycash

--- 28_.php
+++ 28_.php
@@ -1,14 +1,21 @@
 <?php //Conectar con una Base de datos
 
-$servidor = "localhost"; // = 127.0.0.1
-$usuario = "root";
+$servidor = "localhost"; // Este servidor es igual a = 127.0.0.1
+$user = "root";
 $password = "";
 
-try{
+try{   // Trycash es un controlador de errores 
 
+    $conexion=new PDO("mysql:host=$servidor;dbname=album", $user, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    //ARRT_ERRMODE Es una constante estática 
+    //ERRMODE_EXCEPTION Son los datos que necesita setAttribute para mostrar los errores
+    echo "Conexión establecida";
+
+}catch(PDOException $error){
+    echo "Conexión erronea";
 }
-catch() {
-    
-}
+
 
 ?>
